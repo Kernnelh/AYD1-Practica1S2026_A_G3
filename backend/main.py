1a3f928ce83362f096b0df839a5bce105fc27344
joseesgu <joseesgutierrez12@gmail.com>
2026-02-20 15:45:02 -0600

    feat(database): establecer conexión con MySQL usando SQLAlchemy
    Se implementa el motor de base de datos para el backend.
    
    Detalles:
    - Creación de database.py con la configuración de SQLAlchemy y la fábrica de sesiones (SessionLocal).
    - Integración de python-dotenv para la lectura segura de credenciales desde el archivo .env local.
    - Adición de un endpoint temporal (/test-db) en main.py para validar la conexión exitosa al servidor MySQL local.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from database import engine
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"mensaje": "¡El backend de NoteCraft está vivo y funcionando!"}
+
+@app.get("/test-db")
+def test_db_connection():
+    try:
+        # Intentamos abrir una conexión rápida al motor
+        with engine.connect() as connection:
+            return {"mensaje": "¡Conexión a MySQL exitosa"}
+    except Exception as e:
+        return {"error": f"Falló la conexión: {str(e)}"}
